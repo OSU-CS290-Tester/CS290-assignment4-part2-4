e66052208e19aeca0d93792ea329163cf6a33f64
michealwillard <willardm@onid.oregonstate.edu>
2015-02-14 19:49:28 -0800

    Setup DB through GUI, got addItem function working, got DB display functionality working, framed out other functions.

--- interface.php
+++ interface.php
@@ -5,10 +5,9 @@ include 'storedInfo.php';
 
 $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "willardm-db", $myPassword, "willardm-db");
 
-if(!$mysqli || $mysqli->connect_errno){
+if (!$mysqli || $mysqli->connect_errno){
   echo "Connnection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
 }
-
 ?>
 <!DOCTYPE html>
 <!--
@@ -24,26 +23,29 @@ Assignment 4 Part 2
   <title>Assignment 4 Part 2</title>
 </head>
 <body>
+  <!-- ************** -->
   <form method="POST" id="in_video" action="tableRefresh.php">
+    <!-- *********** -->
     <fieldset>
       <legend>Add Video</legend>
       <p>
+        <!-- <input type="hidden" name="action" value="add"/> -->
         <label>Name (required):</label>
-        <input type="text" name="in_name"/>
+        <input type="text" name="in_name" maxlength="255"/>
         <label>Category:</label>
-        <input type="text" name="in_category"/>
+        <input type="text" name="in_category" maxlength="255"/>
         <label>Length (in minutes):</label>
-        <input type="text" name="in_length"/>
+        <input type="number" name="in_length" min="1" max="400"/>
       </p>
-      <input type="submit" name="in_submit" value="Add to Database"/>
+      <input type="submit" name="add_item" value="Add to Database"/>
     </fieldset>
   </form>
   <p>
     <h2>Video Database</h2>
-    <form method="POST">
+    <form method="POST" action='interface.php' form='category'>
       <legend>Filter by Category:</legend>
-      <select>
-        <option name="filter_category" value="All Movies">All Movies</option>
+      <select name ="filter_category">
+        <option>All Movies</option>
         <?php
         // Fetch the categories from the DB
         $get_categories = "SELECT DISTINCT category FROM movieDB";
@@ -55,8 +57,10 @@ Assignment 4 Part 2
         $result->close();
         ?>
       </select>
+      <input type="submit" id="category" name="filter_category" value="Apply Filter">
     </form>
   </p>
+  <!-- CREATE THE TABLE -->
 <table border="1">
   <tr>
     <!--<td>ID</td>-->
@@ -68,35 +72,58 @@ Assignment 4 Part 2
   </tr>
 
   <?php
-  $out_id    = NULL;
-  $out_name = NULL;
-  $out_category = NULL;
-  $out_length = NULL;
-  $out_avail = NULL; //0 for available, 1 for checked out
+  $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "willardm-db", $myPassword, "willardm-db");
 
-  if (!$stmt->bind_result($out_id, $out_name, $out_category, $out_length, $out_avail)) {
-      echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+  if (!$mysqli || $mysqli->connect_errno){
+    echo "Connnection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
   }
 
-  while ($stmt->fetch()) {
-    if($out_avail === 0){
-      $out_text = 'Available';
+  // if (!($tableOut = $mysqli->prepare("SELECT id, name, category, length, rented FROM movieDB"))) {
+  //     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+  // }
+  //
+  // if (!$tableOut->execute()) {
+  //     echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+  // }
+  //
+  // $out_id    = NULL;
+  // $out_name = NULL;
+  // $out_category = NULL;
+  // $out_length = NULL;
+  // $out_avail = NULL; //0 for available, 1 for checked out
+  //
+  // if (!$tableOut->bind_result($out_id, $out_name, $out_category, $out_length, $out_avail)) {
+  //     echo "Binding output parameters failed: (" . $stmt->errno . ") " . $tableOut->error;
+  // }
+  $queryStmt = "SELECT id, name, category, length, rented FROM movieDB";
+  $tableOut = $mysqli->query($queryStmt);
+
+  if ($tableOut->num_rows > 0){
+    while ($row = $tableOut->fetch_row()) {
+      if ($row[4] === '0'){
+        $out_text = 'Available';
+      }
+      elseif ($row[4] === '1'){
+        $out_text = 'Checked Out';
+      }
+      $idVal = $row[0];
+      echo "<tr><td>" . $row[1] .
+      "</td><td>". $row[2] .
+      "</td><td>". $row[3] .
+      "</td><td>". $out_text . $row[4];
+      echo "<form action='tableRefresh.php method='POST'>
+      <input type='hidden' name='id' value='$idVal'>
+      <input type='submit' name='change_status' value='Change Status'>
+      </form></td>";
+      echo "<form action='tableRefresh.php method='POST'>
+      <input type='hidden' name='id' value='$idVal'>
+      <td><input type='submit' name='delete_entry' value='Delete Entry'>
+      </form></td>";
     }
-    elseif($out_avail === 1){
-      $out_text = 'Checked Out';
-    }
-      printf("<tr>
-      <td>%s</td>
-      <td>%s</td>
-      <td>%i</td>
-      <td>%s
-      <button type=\"submit\" name=\"change_status\" value=\"{$out_id}\">
-      Check In/Out</button></td>
-      <td><button type=\"submit\" name=\"delete_entry\" value=\"{$out_id}\">
-      Delete</button></td>
-      ", $out_name, $out_category, $out_length, $out_text);
   }
-  $stmt->close();
+  else{
+    echo "The database is empty";
+  }
   ?>
 </table>
 </body>
--- tableRefresh.php
+++ tableRefresh.php
@@ -1,14 +1,84 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors','On');
-include 'storedInfo.php';
+// storedInfo Password only works on one page?
+// include 'storedInfo.php';
 // Micheal Willard
 // Oregon State University
 // CS 290-400
 // Winter 2015
 // Assignment 4 Part 2
 
+$filePath = explode('/', $_SERVER['PHP_SELF'], -1);
+$filePath = implode('/', $filePath);
+$redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath;
+$redirect1 = $redirect . '/interface.php';
+$goBackText = "<p>Click <a href=\"$redirect1\">here</a> to go back.";
 
+// Parse the POST request to check which function was requested
+if(isset($_POST["add_item"])){
+  // echo "Adding Item";
+  addItem($_POST["in_name"], $_POST["in_category"], $_POST["in_length"]);
+}
+if(isset($_POST["change_status"])){
+  changeStatus($_POST["id"]);
+}
+if(isset($_POST["delete_entry"])){
+  deleteEntry($_POST["id"]);
+}
 
+//*****  FUNCTION DEFINITIONS *****
+
+function addItem($varName, $varCat, $varLen){
+  $filePath = explode('/', $_SERVER['PHP_SELF'], -1);
+  $filePath = implode('/', $filePath);
+  $redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath;
+  $redirect1 = $redirect . '/interface.php';
+  $goBackText = "<p>Click <a href=\"$redirect1\">here</a> to go back.";
+  // Check for Required Fields
+  if($varName === NULL){
+    echo "The <b>Name</b> field was left blank.<br>";
+    echo $goBackText;
+  }
+  elseif($varCat === NULL){
+    echo "The <b>Category</b> field was left blank.<br>";
+    echo $goBackText;
+  }
+  elseif($varLen === NULL){
+    echo "The <b>Length</b> field was left blank.<br>";
+    echo $goBackText;
+  }
+  else{
+    // Connect to the DB
+    $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "willardm-db", "d5F4Pqrfv3YF9nBt", "willardm-db");
+
+    if(!$mysqli || $mysqli->connect_errno){
+      echo "Connnection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+    }
+    // Connection Passed
+
+    if(!$stmtAdd = $mysqli->prepare("INSERT INTO movieDB(name, category, length) VALUES (?, ?, ?)")){
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    /* Prepared statement, stage 2: bind and execute */
+    if (!$stmtAdd->bind_param("ssi", $varName, $varCat, $varLen)) {
+      echo "Binding parameters failed: (" . $stmtAdd->errno . ") " . $stmtAdd->error;
+    }
+
+    if (!$stmtAdd->execute()) {
+      echo "Execute failed: (" . $stmtAdd->errno . ") " . $stmtAdd->error;
+    }
+    echo "This Film was added to the database.";
+    echo "<p>Click <a href=\"$redirect1\">here</a> to go back.";
+  }
+}
+
+function changeStatus($varId){
+
+}
+
+function deleteEntry($varId){
+
+}
 
 ?>
